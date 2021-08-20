400fcab54737eb5877027f4b7ab0f1853e857ae4
Marat Kotik <mkotik@Marats-MBP.fios-router.home>
2021-08-20 11:44:31 -0400

    added functionality to saveEdit and deleteColor

--- src/components/BubblePage.js
+++ src/components/BubblePage.js
@@ -2,26 +2,53 @@ import React, { useEffect, useState } from "react";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
-import fetchColorService from '../services/fetchColorService';
+import fetchColorService from "../services/fetchColorService";
+import axiosWithAuth from "../helpers/axiosWithAuth";
 
 const BubblePage = () => {
   const [colors, setColors] = useState([]);
   const [editing, setEditing] = useState(false);
+
+  useEffect(() => {
+    fetchColorService(setColors);
+  }, []);
 
   const toggleEdit = (value) => {
     setEditing(value);
   };
 
   const saveEdit = (editColor) => {
+    const id = editColor.id;
+    axiosWithAuth()
+      .put(`http://localhost:5000/api/colors/${id}`, editColor)
+      .then((res) => {
+        fetchColorService(setColors);
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
   };
 
   const deleteColor = (colorToDelete) => {
+    const id = colorToDelete.id;
+    axiosWithAuth()
+      .delete(`http://localhost:5000/api/colors/${id}`)
+      .then((res) => {
+        fetchColorService(setColors);
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
   };
 
   return (
     <div className="container">
-      <ColorList colors={colors} editing={editing} toggleEdit={toggleEdit} saveEdit={saveEdit} deleteColor={deleteColor}/>
-      <Bubbles colors={colors}/>
+      <ColorList
+        colors={colors}
+        editing={editing}
+        toggleEdit={toggleEdit}
+        saveEdit={saveEdit}
+        deleteColor={deleteColor}
+      />
+      <Bubbles colors={colors} />
     </div>
   );
 };
--- src/services/fetchColorService.js
+++ src/services/fetchColorService.js
@@ -1,7 +1,13 @@
-import axiosWithAuth from '../helpers/axiosWithAuth';
+import axiosWithAuth from "../helpers/axiosWithAuth";
 
-const fetchColorService = () => {
-    
-}
+const fetchColorService = (setColors) => {
+  axiosWithAuth()
+    .get(`http://localhost:5000/api/colors`)
+    .then((res) => {
+      console.log(res);
+      setColors(res.data);
+    })
+    .catch((err) => console.log(err));
+};
 
 export default fetchColorService;
